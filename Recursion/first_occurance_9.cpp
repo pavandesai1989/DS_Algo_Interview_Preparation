b27748660cd719e6674745b58bf987ab040e0cc9
Pavan R. Mutalikdesai <60208890+pavandesai1989@users.noreply.github.com>
2022-04-30 19:28:27 +0530

    Update first_occurance_9.cpp

--- Recursion/first_occurance_9.cpp
+++ Recursion/first_occurance_9.cpp
@@ -1,1 +1,15 @@
 
+int firstIndex(int input[], int size, int x) {
+  /* Don't write main().
+     Don't read input, it is passed as function argument.
+     Return output and don't print it.
+     Taking input and printing output is handled automatically.
+  */
+    if (size == 0)  return -1;
+	if (input[0] == x)  return 0;
+
+	int index = firstIndex(input+1,size-1,x);
+
+	return (index == -1) ? -1 : index + 1;
+
+}
