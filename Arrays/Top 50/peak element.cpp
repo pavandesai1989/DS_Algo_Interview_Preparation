ef194a42178219b46d8e1cdb604a141eaa4142b6
Pavan R. Mutalikdesai <60208890+pavandesai1989@users.noreply.github.com>
2022-07-28 21:33:32 +0530

    Create peak element.cpp

---
+++ Arrays/Top 50/peak element.cpp
@@ -0,0 +1,18 @@
+****BRUTE FORCE****
+Time Complexity
+O(n), Where ‘n’ denotes the number of elements present in the sequence.
+
+ 
+
+We need to traverse the array once to find the peak element hence the time complexity is of the order of ‘n’.
+
+Space Complexity
+O(1). We are using constant space
+
+******OPTIMAL*****
+Time Complexity
+O(log(n)), ‘n’ is the length of the array.
+
+ 
+
+The binary search takes time of the order of log (n)
